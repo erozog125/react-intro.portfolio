b577c03abdad44fc5cfc5bf3db5ed08838714490
Edwin Rozo - edwindev <erozog125@gmail.com>
2023-08-08 07:26:43 -0500

    Add Routes with react-router-com

--- src/components/ItemNavbar/ItemNavbar.jsx
+++ src/components/ItemNavbar/ItemNavbar.jsx
@@ -3,6 +3,6 @@ import { NavLink } from 'react-router-dom'
 
 export const ItemNavbar = ({ content, route}) => {
   return (
-    <li><NavLink className='hover:text-slate-300 shadow-xl' to={route}>{content}</NavLink></li>    
+    <li><NavLink activeClassName='active-link' className='hover:text-slate-300 shadow-xl' to={route}>{content}</NavLink></li>    
   )
 }
